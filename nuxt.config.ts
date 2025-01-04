dcb9d5b8e7ac8ead063a657891265bae3e59f7cf
Nikita Alexov <polarbeararmor@gmail.com>
2025-01-04 21:05:43 +0400

    chore: initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  srcDir: 'src',
+  dir: {
+    app: 'src/app',
+    middleware: 'src/app',
+    layouts: 'src/app',
+    plugins: 'src/app',
+    assets: 'src/app'
+  },
+  typescript: {
+    typeCheck: true
+  },
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4,
+  },
+})
