67c41e8b775b2974e2755bdceeaa5977a3388a8b
Nikita Alexov <polarbeararmor@gmail.com>
2025-01-19 20:22:20 +0400

    feat: add api plugin

---
+++ src/app/plugins/api.ts
@@ -0,0 +1,68 @@
+import type { ApiError } from '@/shared/model/interfaces/error.interface'
+import { FetchError } from 'ofetch'
+
+export default defineNuxtPlugin(() => {
+  const api = $fetch.create({})
+
+  function setResponseError(err: any) {
+    console.error(err)
+    if (err instanceof FetchError) {
+      return {
+        error: true,
+        data: err.data,
+        status: err.status,
+        message: err.message,
+      }
+    }
+    return {
+      error: true,
+      message: 'Unknown error',
+    }
+  }
+
+  const apiGet = async <T>(url: string, options?: any): Promise<T | ApiError> => {
+    try {
+      return await api(url, { method: 'GET', ...options })
+    }
+    catch (err) {
+      return setResponseError(err)
+    }
+  }
+
+  const apiPost = async <T>(url: string, body: any, options?: any): Promise<T | ApiError> => {
+    try {
+      return await api(url, { method: 'POST', body, ...options })
+    }
+    catch (err) {
+      return setResponseError(err)
+    }
+  }
+
+  const apiPatch = async <T>(url: string, body: any, options?: any): Promise<T | ApiError> => {
+    try {
+      return await api(url, { method: 'PATCH', body, ...options })
+    }
+    catch (err) {
+      return setResponseError(err)
+    }
+  }
+
+  const apiDelete = async <T>(url: string, options?: any): Promise<T | ApiError> => {
+    try {
+      return await api(url, { method: 'DELETE', ...options })
+    }
+    catch (err) {
+      return setResponseError(err)
+    }
+  }
+
+  return {
+    provide: {
+      api,
+      apiGet,
+      apiPost,
+      apiPatch,
+      apiDelete,
+    },
+  }
+})
