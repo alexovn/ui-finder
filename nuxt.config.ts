87a07663ccc5a87d06c6ab5b41f3dc84116ceae4
Nikita Alexov <polarbeararmor@gmail.com>
2025-07-16 00:48:31 +0400

    feat: add script for starting app in dev mode with prod env variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,7 +24,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_API_BASE,
+      apiBase: process.env.NUXT_API_BASE || 'http://localhost:3333',
       githubToken: process.env.NUXT_PUBLIC_GITHUB_TOKEN
     }
   },
