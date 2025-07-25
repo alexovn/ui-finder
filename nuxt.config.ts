852098c1222530599f93aa8866ed0e14769718db
Nikita Alexov <polarbeararmor@gmail.com>
2025-07-25 22:54:00 +0400

    fix: update apiBase env variable name
    There was mistake in env variable name, that didn't allow to use
    production API.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,8 +24,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_API_BASE || 'http://localhost:3333',
-      githubToken: process.env.NUXT_PUBLIC_GITHUB_TOKEN
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:3333',
     }
   },
 
