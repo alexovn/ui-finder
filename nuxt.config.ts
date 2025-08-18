d51894fe3395a6d71ae66d9e3c955546dae02356
Nikita Alexov <polarbeararmor@gmail.com>
2025-08-18 13:07:06 +0400

    feat(SEO): update app title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
       },
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1, minimum-scale=1, user-scalable=no, shrink-to-fit=no, viewport-fit=cover',
-      title: 'UI Finder | The One-Stop Solution for Finding UI Libraries',
+      title: 'UI Finder | Library search',
       link: [
         { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
       ],
--- src/app/app.vue
+++ src/app/app.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 useSeoMeta({
-  title: 'UI Finder | The One-Stop Solution for Finding UI Libraries',
-  ogTitle: 'UI Finder | The One-Stop Solution for Finding UI Libraries',
+  title: 'UI Finder | Library search',
+  ogTitle: 'UI Finder | Library search',
   description: 'UI Finder is a powerful tool that helps developers quickly discover the best UI libraries for their framework. Save time by filtering, sorting, and finding up-to-date, high-quality UI solutions in seconds. No more endless web searches—just the right libraries at your fingertips!',
   ogDescription: 'UI Finder is a powerful tool that helps developers quickly discover the best UI libraries for their framework. Save time by filtering, sorting, and finding up-to-date, high-quality UI solutions in seconds. No more endless web searches—just the right libraries at your fingertips!',
 })
