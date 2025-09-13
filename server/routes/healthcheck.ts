b14d73a2dd51f84fbb205822cf4d659e786c2be3
Nikita Alexov <polarbeararmor@gmail.com>
2025-09-13 14:39:19 +0400

    feat: simplified Nuxt healthcheck handler

--- server/routes/healthcheck.ts
+++ server/routes/healthcheck.ts
@@ -1,15 +1,3 @@
 import { defineEventHandler } from 'h3'
 
-export default defineEventHandler(() => {
-  try {
-    return {
-      status: 'ok',
-    }
-  }
-  // eslint-disable-next-line unused-imports/no-unused-vars
-  catch (error: any) {
-    return {
-      status: 'error',
-    }
-  }
-})
+export default defineEventHandler(() => ({ status: 'ok' }))
